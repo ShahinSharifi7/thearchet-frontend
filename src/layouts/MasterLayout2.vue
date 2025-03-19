91da090cb8cb7dc6e21fc8fb22fe776a8469d1c8
Shahin Sharifi <75165591+ShahinSharifi7@users.noreply.github.com>
2025-03-19 13:14:58 -0400

    tried to fix the scroll making bottom navbar come to the middle of the screen

--- src/layouts/MasterLayout2.vue
+++ src/layouts/MasterLayout2.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="flex justify-center items-center min-h-screen bg-[#fdeae7]">
+  <div class="flex justify-center items-center min-h-screen bg-[#fdeae7] overflow-hidden">
     <!-- Phone frame only on large screens -->
     <div
         class="relative w-[390px] h-[844px] bg-black rounded-[40px] shadow-2xl overflow-hidden flex flex-col"
         v-if="isLargeScreen"
     >
-      <div class="flex-1 overflow-y-auto">
+      <div class="flex-1 overflow-y-auto" @touchmove.prevent>
         <router-view class="bg-white"/>
       </div>
       <!-- Bottom Navbar inside phone frame -->
-      <BottomNavbar v-if="!hideNavbar" class="w-full z-30"/>
+      <BottomNavbar v-if="!hideNavbar" class="w-full fixed bottom-0 z-30"/>
     </div>
 
     <!-- Fullscreen view on smaller screens -->
-    <div class="w-full h-screen flex flex-col" v-else>
-      <div class="flex-1 overflow-y-auto relative">
+    <div class="w-full h-screen flex flex-col overflow-hidden" v-else>
+      <div class="flex-1 overflow-y-auto relative" @touchmove.prevent>
         <router-view class="bg-white"/>
       </div>
       <BottomNavbar v-if="!hideNavbar" class="fixed bottom-0 w-full z-30"/>
@@ -38,9 +38,11 @@ const hideNavbar = computed(() => route.path === '/login' || route.path === '/si
 
 onMounted(() => {
   window.addEventListener('resize', updateScreenSize);
+  document.body.style.overflow = 'hidden'; // Prevent scrolling
 });
 
 onUnmounted(() => {
   window.removeEventListener('resize', updateScreenSize);
+  document.body.style.overflow = ''; // Restore scrolling
 });
 </script>
